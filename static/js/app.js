4ed3228468ae937341089eda1b22261fcc4ac571
alimmemon <47302590+alimmemon@users.noreply.github.com>
2020-06-06 20:36:18 -0400

    completed index and app.js

---
+++ static/js/app.js
@@ -0,0 +1,104 @@
+// from data.js
+var table_data = data;
+
+// YOUR CODE HERE!
+
+// Use D3 to select the table
+var table = d3.select("table");
+
+// Use D3 to select the table body
+var tbody = d3.select("tbody");
+
+// Generate HTML Table with all data
+function results(items) {
+    // Iterate through each item object
+    items.forEach((item) => {
+
+        // Append one table row `tr` to the table body
+        var row = tbody.append("tr");
+
+        // Iterate through each key and value
+        Object.entries(item).forEach(([key, value]) => {    
+  
+          // Use the key to determine which array to push the value to
+            if (key === "datetime") {
+                row.append("td").text(value);
+            }
+            else if (key === "city") {
+                row.append("td").text(value);
+            }
+            else if (key === "state") {
+                row.append("td").text(value);
+            }
+            else if (key === "country") {
+                row.append("td").text(value);
+            }
+            else if (key === "shape") {
+                row.append("td").text(value);
+            }
+            else if (key === "duration_minutes") {
+                row.append("td").text(value);
+            }
+            else if (key === "comments") {
+                row.append("td").text(value);
+            }
+        });
+    });
+}
+
+results(table_data);
+
+// Select the button
+var button = d3.select("#filter-btn");
+
+// Select the form
+var form = d3.select("form");
+
+// Create event handlers 
+button.on("click", input);
+form.on("submit", input);
+
+var filtered_data = table_data;
+
+// Complete the event handler function for the form
+function input() {
+
+    // Prevent the page from refreshing so the progress is not lost
+    d3.event.preventDefault();
+    
+    // Create JS Object for multiple input values
+    var input_value = {};
+
+    input_value.datetime = d3.select("#datetime").property("value");
+    input_value.city = d3.select("#city").property("value").toLowerCase();
+    input_value.state = d3.select("#state").property("value").toLowerCase();
+    input_value.country = d3.select("#country").property("value").toLowerCase();
+    input_value.shape = d3.select("#shape").property("value").toLowerCase();
+    input_value.duration_minutes = d3.select("#duration_minutes").property("value");
+    
+    
+    // Iterate JS Object
+    var filtered_data = table_data;
+ 
+    // Filter on each input value if one exists
+    Object.entries(input_value).forEach(([key, value]) => {
+        if (value.length > 0) {
+            filtered_data = filtered_data.filter(item => item[key] === value); 	
+            console.log(filtered_data);
+        }
+
+    });
+     console.log(filtered_data);
+
+    // Clears HTML table body and provide filtered results
+    tbody.html("");
+    results(filtered_data);
+      
+};
+
+
+
+
+
+
+
